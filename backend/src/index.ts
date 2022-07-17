e55bb15244df33dc6b1af60f95727bc3f9abb427
Stefan Chen <zchen158@my.bcit.ca>
2022-07-16 18:03:35 -0700

    Create index.ts and import libraries

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import passport from 'passport';
+import passportLocal from 'passport-local';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import bcrypt from 'bcryptjs';
+import User from './User'
+import dotenv from 'dotenv';
