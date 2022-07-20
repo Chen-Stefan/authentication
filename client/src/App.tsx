1166920515bb238bb0961da471124d029da521bf
Stefan Chen <zchen158@my.bcit.ca>
2022-07-19 20:04:27 -0700

    Use react-router-dom to build 4 routes for 4 pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Homepage from './pages/HomePage';
+import AdminPage from './pages/AdminPage';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
 
+// Routes就是v5的 Switch
 function App() {
   return (
-   <div></div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/admin" element={<AdminPage />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
