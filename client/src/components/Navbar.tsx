ed0b3f302e9d84470f301bf3655f70f2921e42ed
Stefan Chen <zchen158@my.bcit.ca>
2022-07-19 20:26:23 -0700

    Create links for Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div>
-      
+    <div className="NavContainer">
+      <Link to="/logout">Logout</Link>
+      <Link to="/">Home</Link>
+      <Link to="/profile">Profile</Link>
+      <Link to="/admin">Admin</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
     </div>
-  )
+  );
 }
